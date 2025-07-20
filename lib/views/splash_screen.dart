b0104076b39bd22fe3b88110895914a5889bf1a4
hebabo <hebakhaledsalem223@gmail.com>
2025-07-21 00:56:19 +0300

    add screenshots

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -14,7 +14,7 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
 
     // Delay for 2 seconds then navigate to LoginScreen
-    Future.delayed(const Duration(seconds: 2), () {
+    Future.delayed(const Duration(seconds: 10), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const LoginScreen()),
@@ -27,10 +27,10 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       body: Center(
         child: Image.asset(
-          'assets/images/splash.jpg', 
-          width: 200,
-          height: 200,
-          fit: BoxFit.contain,
+          'assets/images/splash.png',
+          // width: 200,
+          // height: 200,
+          fit: BoxFit.cover,
         ),
       ),
     );
